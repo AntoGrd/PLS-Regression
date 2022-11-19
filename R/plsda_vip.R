d943b4d66fa152758d0ab5e056046c6c88e5ad2a
Laura-Boutonnet <62285953+Laura-Boutonnet@users.noreply.github.com>
2022-11-19 18:44:16 +0100

    Update plsda_vip.R

--- R/plsda_vip.R
+++ R/plsda_vip.R
@@ -37,15 +37,3 @@ plsda.vip<-function(PLS,threshold=0.8){
   class(instance) <- "VIP"
   return(instance)
 }
-
-plot.vip<-function(VIP){
-  if (class(VIP) != "VIP") {
-    stop("object must be a VIP !")
-  }
-  titre=paste("Variable Importance in Projection ( *:VIP>",instance$threshold,")")
-  plot_VIP=barplot(as.matrix(t(instance$VIP)),main=titre,col="blue",ylim=c(0.0,max(instance$VIP)+0.3),xlab="Variable",ylab="VIP")
-  seuil=NULL
-  seuil[instance$VIP[,1]>instance$threshold]='*'
-  text(plot_VIP,t(instance$VIP)+0.15,seuil,cex=2)
-  abline(instance$threshold,0,col = "red")
-}
