fa06c960e36d202c0bfcba4c3f376aab217cbd3b
Robin_Ponson <83652394+Rocksorr@users.noreply.github.com>
2022-11-24 18:02:04 +0100

    Scree plot amélioré

--- R/plsda_scree_plot.R
+++ R/plsda_scree_plot.R
@@ -1,30 +1,17 @@
 plsda_scree_plot=function(obj){
   
-  # Nombre de composantes
-  
-  ncomp=obj$ncomp
-  
   # Calcul des corrélations 
   
   X=obj$X
   corX=cor(X)
   
   # Calcul des valeurs propres
+  eigenvalues=eigen(corX)$values
+  cols <- ifelse(eigenvalues > 1, "purple", "yellow")
   
-  eigenvalues=eigen(corX)
-  eigenvalues1=eigenvalues$values[1:ncomp]
-  scree=plot(1:ncomp,eigenvalues1,type="b",ylab="Valeurs propres",xlab="Composantes", main="Scree plot")
-  
-  cor.factors <- NULL
-  
-  for (j in 1:ncol(X)){
-    rf <- sqrt(eigenvalues$values[j])*eigenvalues$vectors[,j]
-    cor.factors <- cbind(cor.factors,rf)
-  }
-  
-  rownames(cor.factors)=colnames(X)
-  colnames(cor.factors)=paste("F",1:ncol(X),sep="")
-  plot(1:ncomp,cor.factors,type="b",ylab="Valeurs propres",xlab="Composante",main="Scree plot")
+  # Scree plot
+  scree <- barplot(eigenvalues, col = cols, ylab="Valeurs propres", xlab="Composantes", main="Scree plot")
+  points(x = scree, y = eigenvalues, type = "o", pch = 16)
   
 }
 data(iris)
