6d7d84c97f74db1d982eca6c792c55d81eaf61db
Robin_Ponson <83652394+Rocksorr@users.noreply.github.com>
2022-11-24 14:30:17 +0100

    Ajout de la sélection de variables

--- R/plsda_vip_test.R
+++ R/plsda_vip_test.R
@@ -1,8 +1,13 @@
 plsda.vip<-function(PLS,threshold=0.8){
   
+  # Récupération des valeurs nécéssaires à l'algorithme 
+  
   q=PLS$Y_loadings
   t=PLS$X_scores
   w=PLS$weights
+  X=PLS$X
+  y=PLS$y
+  ncomp=PLS$ncomp
   
   p=nrow(w)
   h=ncol(w)
@@ -13,7 +18,22 @@ plsda.vip<-function(PLS,threshold=0.8){
   weigth=(w/sqrt(colSums(w^2)))^2
   vip = sqrt(p*(ssy*weigth)/tot_ssy)
   
+  #détermination des variables importantes
+  variable_importante=rownames(vip)[which(vip[,ncomp]>threshold)]
+  
+  
+  # Si une seule variable importante, on sélectionne les 2 variables avec le plus haut VIP
+  if (length(variable_importante)<2){
+    vip_sorted = vip[order(-vip[,ncomp]),]
+    variable_importante=rownames(vip_sorted)[1:2]
+  }
+  # Création d'un nouveau dataset avec uniquement les variables importantes
+  newX = X[,variable_importante]
+  
   PLS$vip=vip
   PLS$tresholds=threshold
-  return(PLS)
+  return(newX)
+  
 }
+
+plsda.vip(res)
