d0433b4d16bf70449b419445688d7fa9954d637c
AntoGrd <antoinegiroud.work@gmail.com>
2022-12-03 13:19:58 +0100

    suppression error

--- R/PLSDA.plot.R
+++ R/PLSDA.plot.R
@@ -19,12 +19,16 @@
 #' PLSDA::explanatory_variables(var1 = iris$Sepal.Length,var2=iris$Sepal.Width,color=iris$Species)
 
 explanatory_variables_plot=function(var1,var2, color){
-  
   verify=require("plotly")
   if(verify!=TRUE){
     install.packages("plotly")
     verify=TRUE
   }
-  
-  plot_ly(x=var1,y=var2, color=color)
+  plot_ly(x=var1,y=var2, color=color,type="scatter",mode="markers")%>% layout(title="Scatter plot",
+                                                hovermode='closest',dragmode= 'select',
+                                                legend=list(title=list(text='Color')),
+                                                xaxis = list(
+                                                  title = 'Var 1'),
+                                                yaxis = list(
+                                                  title='Var 2'))
 }
